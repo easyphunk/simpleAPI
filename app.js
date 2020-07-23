42dc87ee936ee6237a06182807f2d26c34896460
easyphunk <46828328+easyphunk@users.noreply.github.com>
2020-07-23 17:34:08 +0300

    fix: remove unused code

--- app.js
+++ app.js
@@ -14,13 +14,7 @@ if (process.env.NODE_ENV === 'development') {
 app.use(cors());
 app.use(express.json());
 
-app.use((req, res, next) => {
-    req.requestTime = new Date().toISOString();
-    next();
-});
-
 // ROUTES
-
 app.use('/api/v1/trips', tripRouter);
 app.use('/api/v1/users', userRouter);
 
