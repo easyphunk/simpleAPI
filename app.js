1de3e1273d16ef587a382359d83eb491924c5ae9
easyphunk <46828328+easyphunk@users.noreply.github.com>
2020-07-24 23:17:22 +0300

    feat: implement ndb and manage unhandled routs

--- app.js
+++ app.js
@@ -18,4 +18,11 @@ app.use(express.json());
 app.use('/api/v1/trips', tripRouter);
 app.use('/api/v1/users', userRouter);
 
+app.all('*', (req, res, next) => {
+    res.status(404).json({
+        status: 'fail',
+        message: `Cannot find ${req.originalUrl} on this server!`
+    })
+})
+
 module.exports = app;
