45b085b37633e9de42ae62423be0467b386dc74c
easyphunk <46828328+easyphunk@users.noreply.github.com>
2020-07-29 19:10:37 +0300

    feat: expose header for authorization

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
-app.use(cors());
+app.use(cors({
+    exposedHeaders: 'Authorization'
+}));
 app.use(express.json());
 
 // ROUTES
